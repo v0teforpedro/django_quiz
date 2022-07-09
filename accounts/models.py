0955479ddcc49c8c2af533f92ddf91234512f450
Peter Sushkin <sushkin.p@gmail.com>
2022-07-09 22:10:49 +0300

    added signal to add newly created users to user-groups

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,7 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class CustomUser(AbstractUser):
@@ -13,3 +15,9 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+@receiver(post_save, sender=CustomUser)
+def move_user_to_group(sender, instance, created, **kwargs):
+    if created and not instance.is_superuser:
+        instance.groups.add(Group.objects.get(name='Users'))
